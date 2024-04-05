df6c02685a2b8044f8f060f25dcaf49e2450ffcc
Francesco Tassi <tassi.francesco@gmail.com>
2024-04-05 19:36:25 +0200

    Add neotree as file tree plugin

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,12 +1,15 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    version = "*",
-    dependencies = {
-        "nvim-lua-plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim"
-    },
-    config = function ()
-        require("neo-tree").setup {}
-    end,
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {}
+
+    vim.keymap.set('n', '<leader>tt', '<Cmd>:Neotree float toggle<CR>', { desc = 'Toggle file tree' })
+    vim.keymap.set('n', '<leader>tc', '<Cmd>:Neotree reveal_file=%:p reveal_force_cwd<CR>', { desc = 'Reveal current buffer on file tree' })
+  end,
 }
