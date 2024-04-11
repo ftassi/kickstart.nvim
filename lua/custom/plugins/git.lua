126c35024b7706a20f197d657c91360d68560238
Francesco Tassi <tassi.francesco@gmail.com>
2024-04-11 08:49:52 +0200

    Add a TODO for removing gv.vim

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -17,6 +17,9 @@ return {
       vim.keymap.set('n', '<leader>gc', '<Cmd>:Git commit<CR>', { desc = '[G]it [C]ommit' })
     end,
   },
+  -- TODO: This could be removed mapping fugitive git log with the right format
+  -- fugitive allows for interactive rebase from the log which gv.vim seems no be able to
+  -- the alternative could be add a `ri` mapping for that view, getting the hashcommit from the logline
   {
     'junegunn/gv.vim',
     dependencies = {
